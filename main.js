bace196df3b398636a747521ba0f1faad25e3285
Zeng Xiangfei <17204129@stu.nchu.edu.cn>
2018-10-06 21:34:51 +0800

    Init

---
+++ main.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         破解超星作业比较器粘贴限制
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @author       Zeng Xiangfei
+// @homepage     https://github.com/zxf1023818103/enable-chaoxing-editor-paste
+// @supportURL   https://github.com/zxf1023818103/enable-chaoxing-editor-paste/issues
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   edge
+// @compatible   safari
+// @license      MIT
+// @match        https://mooc1-2.chaoxing.com/work/doHomeWorkNew?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    for (var editorName in UE.instants) { UE.instants[editorName].__allListeners.beforepaste = null }
+})();
